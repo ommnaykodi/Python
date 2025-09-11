bacb6c678cb237db35d5e89e34eeb07554a1bad4
Om Mahendra Naykodi <ommnaykodi@gmail.com>
2025-09-11 23:29:40 +0530

    staring my 1st program in python

---
+++ sept-11.py
@@ -0,0 +1,27 @@
+print("helloworld")
+
+print("sept-11")
+
+a=12
+b=3
+print("before a",a)
+print("before b",b)
+c=a
+a=b
+print("after conversion a value",a)
+b=c
+print("after conversion b valu",b)
+
+
+
+
+a= 2
+b=3
+print("before a",a)
+print ("befor b",b)
+a=a+b
+
+b=a-b
+print("after b",b)
+a=a-b
+print("after a",a)
