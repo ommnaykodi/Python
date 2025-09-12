04bdddc745c41dca04450f32e9776d1ea3df3a98
Om Mahendra Naykodi <ommnaykodi@gmail.com>
2025-09-12 22:13:10 +0530

    day 2

---
+++ sept-12.py
@@ -0,0 +1,39 @@
+print("this is my second day")
+
+
+#aditon
+a = 5
+b = 6
+print(a+b)
+
+a = 12
+b = 16
+c = a+b
+print(c)
+
+a = 2
+b = 4
+c = a
+a=b
+b=c
+print(a)
+print(b)
+
+a=input(int)
+print(a)
+
+a=2
+b=3
+a=a+b
+b=a-b
+a=a-b
+print(a)
+print(b)
+ 
+x=10
+y=20
+if x > y         
+print("x is greater")
+else
+print("y is greater")
+
