61468b02190c51125672090502f04e0d9ea00047
Om Mahendra Naykodi <ommnaykodi@gmail.com>
2025-09-15 21:42:13 +0530

    Factorial using Do–While Loop (Python doesn’t have do–while, but we can simulate it)

--- sept-15.py
+++ sept-15.py
@@ -38,4 +38,18 @@ for i in range(n, 0, -1):
 
 print("Factorial =", fact)
 
+print("Factorial using Do–While Loop (Python doesn’t have do–while, but we can simulate it)")
 
+n = int(input("Enter a number: "))
+fact = 1
+i = 1
+
+if n == 0:  # 0! = 1
+    print("Factorial = 1")
+else:
+    while True:
+        fact *= i
+        i += 1
+        if i > n:
+            break
+    print("Factorial =", fact)
