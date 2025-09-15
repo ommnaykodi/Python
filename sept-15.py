64a71bd3084d05933fbb7093f247c58185343a7e
Om Mahendra Naykodi <ommnaykodi@gmail.com>
2025-09-15 21:35:14 +0530

    Factorial using While Loop (Decrement)

--- sept-15.py
+++ sept-15.py
@@ -8,3 +8,14 @@ while i <= n:
     i += 1
 
 print("Factorial =", fact)
+
+print("Factorial using While Loop (Decrement)")
+n = int(input("Enter a number: "))
+fact = 1
+i = n
+
+while i >= 1:
+    fact *= i
+    i -= 1
+
+print("Factorial =", fact)
