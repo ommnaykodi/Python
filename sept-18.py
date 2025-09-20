0e3ee9444dd27180b7413908c4678e4a15216314
Om Mahendra Naykodi <ommnaykodi@gmail.com>
2025-09-20 21:37:19 +0530

    table of any no

---
+++ sept-18.py
@@ -0,0 +1,9 @@
+
+l=int(input("enter a number"))
+i=1
+while i <= 10: 
+  k=l*i
+  i += 1
+  print(k)
+  
+  
---
+++ sept-20.py
@@ -0,0 +1,17 @@
+#table of any no
+k=int(input("enter"))
+i=1
+while i<=10:
+    l=k*i
+    i+=1
+    print(l)
+    
+#greater smaller than
+    
+m=int(input("a enter"))
+n=int(input("a number"))
+if n<m:
+    print(n,"less than",m)
+else:
+    print(m,"less than",n)
+    
